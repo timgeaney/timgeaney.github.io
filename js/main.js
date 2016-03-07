42d611e167e645d20886bad1439f6a35e5d0575c
tim geaney <timgeaney24@gmail.com>
2016-03-07 03:46:30 +0000

    fixed nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 jQuery(document).ready(function($){
+
+  // menu toggle for nav front of site home page
+
+    var navTrigger = document.getElementsByClassName('nav-trigger')[0],
+        body = document.getElementsByTagName('body')[0];
+
+      navTrigger.addEventListener('click', toggleNavigation);
+
+      function toggleNavigation(event) {
+        event.preventDefault();
+        body.classList.toggle('nav-open');
+      }
   
   // form Js
 
